6d1ab1ace35c9d862c7e855ef29e306b1cef9e6c
Rafa <rafasilvasimoes25@gmail.com>
2024-06-12 09:07:20 -0300

    [UPD-add components com Sass]

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,18 +1,30 @@
-import React from "react";
+import React from 'react';
+import './style.scss';
 
 function Lista() {
-    const tarefas = [{ tarefa: 'React', tempo: '02:00:00' }, { tarefa: 'Javascript', tempo: '01:00:00'  }]
-    return (
-      <div>
+  const tarefas = [{
+    tarefa: 'React',
+    tempo: '02:00:00'
+  }, {
+    tarefa: 'Javascript',
+    tempo: '01:00:00'
+  }, {
+    tarefa: "Typescript",
+    tempo: "03:00:00"
+  }];
+  return (
+    <aside className="listaTarefas">
+      <h2> Estudos do dia </h2>
+      <ul>
         {tarefas.map((item, index) => (
-          <div key={index}>
-            <h3> {item.tarefa} </h3>
-            <span> {item.tempo} </span>
-          </div>
+          <li key={index} className="item">
+            <h3>{item.tarefa}</h3>
+            <span>{item.tempo}</span>
+          </li>
         ))}
-      </div>
-    )
-  }
-  
+      </ul>
+    </aside>
+  )
+}
 
 export default Lista;
