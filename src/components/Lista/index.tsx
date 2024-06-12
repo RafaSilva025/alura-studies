36d5fc4d39bc16986ba681e239e72fa407f81cf1
Rafa <rafasilvasimoes25@gmail.com>
2024-06-12 08:32:09 -0300

    [UPD- Renderizando o Array]

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,29 +1,18 @@
 import React from "react";
 
 function Lista() {
+    const tarefas = [{ tarefa: 'React', tempo: '02:00:00' }, { tarefa: 'Javascript', tempo: '01:00:00'  }]
     return (
-      <aside>
-        <h2> Estudos do dia </h2>
-        <ul>
-          <li>
-            <h3>
-              React
-            </h3>
-            <span>
-              02:00:00
-            </span>
-          </li>
-          <li>
-            <h3>
-              Javascript
-            </h3>
-            <span>
-              01:00:00
-            </span>
-          </li>
-        </ul>
-      </aside>
+      <div>
+        {tarefas.map((item, index) => (
+          <div key={index}>
+            <h3> {item.tarefa} </h3>
+            <span> {item.tempo} </span>
+          </div>
+        ))}
+      </div>
     )
   }
+  
 
 export default Lista;
