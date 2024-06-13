ec2183a79ca711e575db5ccdb42d7ad3751c04d2
Rafa <rafasilvasimoes25@gmail.com>
2024-06-13 08:37:48 -0300

    [UPD- Usando CSS modules]

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Formulario from '../components/Formulario';
 import Lista from '../components/Lista';
-import style from './App.module.scss'
+import style from './App.module.scss';
 
 function App() {
   return (
