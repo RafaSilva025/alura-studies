38fbb5ef53248efe0b7b3cde882ab36e51df1fdd
Rafa <rafasilvasimoes25@gmail.com>
2024-06-14 09:12:48 -0300

    [UPD- Refatorando a Lista]

--- src/components/Lista/item/index.tsx
+++ src/components/Lista/item/index.tsx
@@ -1,10 +1,10 @@
-import style from '../Lista.module.scss'
+import style from '../Lista.module.scss';
 
-export default function Item({ tarefa, tempo}: {tarefa: string, tempo: string}) {
-    return (
-     <li key=(index) classname={style.item}>
-       <h3> {item.tarefa} </h3>
-       <span> {item.tempo} </span>
-     </li>
-      }
-  }
+export default function Item({ tarefa, tempo }: { tarefa: string, tempo: string}) {
+  return (
+    <li className={style.item}>
+      <h3>{tarefa}</h3>
+      <span>{tempo}</span>
+    </li>
+  )
+}
