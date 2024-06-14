34a979e9c6643be255e58f824c42fda52ae93661
Rafa <rafasilvasimoes25@gmail.com>
2024-06-14 08:45:25 -0300

    [UPD- Criando o Cronometro]

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import Botao from '../Botao'
-import Relogio from './Relogio'
-import style from './Cronometro.module.scss'
+import Botao from "../Botao";
+import Relogio from "./Relogio";
+import style from './Cronometro.module.scss';
 
 export default function Cronometro() {
-    return (
-        <div className={style.cronometro}>
-           <p className={style.titulo}> Escolha um card e inicie o cronômetro </p>
-        <div className={style.relogioWrapper}>
-           <Relogio />
-           </div>
-           <Botao>
-              Começar!
-           </Botao>
+  return (
+    <div className={style.cronometro}>
+      <p className={style.titulo}>Escolha um card e inicie o Cronômetro</p>
+      <div className={style.relogioWrapper}>
+        <Relogio />
       </div>
-    )
-  }
+      <Botao>
+        Começar!
+      </Botao>
+    </div>
+  )
+}
