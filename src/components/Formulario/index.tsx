268144fe9dd8003755b249f48d572b0848d26a22
Rafa <rafasilvasimoes25@gmail.com>
2024-06-17 08:44:58 -0300

    [UPD- corrigindo formulario]

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -31,7 +31,7 @@ class Formulario extends React.Component {
             step="1"
             name="tempo"
             value={this.state.tempo}
-            onChange=(evento => this.setState({ ...this.state, tempo: evento.target.value}))
+            onChange={evento => this.setState({ ...this.state, tempo: evento.target.value })}
             id="tempo"
             min="00:00:00"
             max="01:30:00"
