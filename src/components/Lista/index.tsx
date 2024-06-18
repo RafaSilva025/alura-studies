b229460f33ed842fd8d0d3f780bfe4dd8045dbb3
Rafa <rafasilvasimoes25@gmail.com>
2024-06-18 09:12:17 -0300

    [UPD- Adicionando uma tarefa]

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,22 +1,23 @@
+import React from 'react';
+import { ITarefa } from '../../types/tarefa';
 import Item from './item';
-import style from './Lista.module.scss;
+import style from './Lista.module.scss';
 
-function Lista( { tarefas }: {tarefas: ITarefa[]}) {
-  
+
+function Lista({ tarefas }: { tarefas: ITarefa[] }) {
   return (
     <aside className={style.listaTarefas}>
       <h2> Estudos do dia </h2>
       <ul>
         {tarefas.map((item, index) => (
           <Item
-          key={index}
-          {...item}
+            key={index}
+            {...item}
           />
         ))}
       </ul>
     </aside>
-  ) 
+  )
 }
-
 
 export default Lista;
