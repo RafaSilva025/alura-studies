afb899f5446456d758d5f25f57f3687f0293ab3e
Rafa <rafasilvasimoes25@gmail.com>
2024-06-18 09:13:29 -0300

    [UPD- Adicionando uma tarefa]

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import style from './Botao.module.scss';
 
-class Botao extends React.Component <{
+class Botao extends React.Component<{
   type?: "button" | "submit" | "reset" | undefined
-}>  {
+}> {
   render() {
-    const { type = 'button' } = this.props;
+    const { type = "button" } = this.props;
     return (
       <button type={type} className={style.botao}>
         {this.props.children}
