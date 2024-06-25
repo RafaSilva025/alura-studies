8f65857dcaf5c90c34b4c780b49baf378988d57a
Rafa <rafasilvasimoes25@gmail.com>
2024-06-25 08:55:57 -0300

    [UPD- iniciando o cronometro]

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -9,21 +9,19 @@ function App() {
   const [tarefas, setTarefas] = useState<ITarefa[]>([]);
   const [selecionado, setSelecionado] = useState<ITarefa>();
 
-  function selecionaTarefa(tarefaSelecionada, ITarefa){
+  function selecionaTarefa(tarefaSelecionada: ITarefa) {
     setSelecionado(tarefaSelecionada);
     setTarefas(tarefasAnteriores => tarefasAnteriores.map(tarefa => ({
-        ...tarefa,
-        selecionado: true
-
-    })));
-}
-
+      ...tarefa,
+      selecionado: tarefa.id === tarefaSelecionada.id ? true : false
+    })))
+  }
   return (
     <div className={style.AppStyle}>
       <Formulario setTarefas={setTarefas} />
-      <Lista 
-      tarefas={tarefas} 
-      selecionaTarefa={selecionaTarefa}
+      <Lista
+        tarefas={tarefas}
+        selecionaTarefa={selecionaTarefa}
       />
       <Cronometro selecionado={selecionado} />
     </div>
